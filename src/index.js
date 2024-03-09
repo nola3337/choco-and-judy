731c854ffff1892d1ecd3f98897567b410b1dcad
Nola <wsxcvbgt5432@gmail.com>
2024-03-09 17:40:21 +0800

    run build already.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import App from "./App";
 import "./styles/main.css";
 import { Provider } from "react-redux";
 import store from "./store/store";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <Router basename="/choco-and-judy">
+      <App />
+    </Router>
   </Provider>
 );
